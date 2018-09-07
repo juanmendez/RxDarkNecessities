979f3d764a5d1fd200ee13b06612849883e72b6e
Juan Mendez <contact@juanmendez.info>
2018-09-07 06:28:45 -0500

    code formatted

--- src/main/kotlin/info/juanmendez/rxstories/model/Song.kt
+++ src/main/kotlin/info/juanmendez/rxstories/model/Song.kt
@@ -1,3 +1,3 @@
 package info.juanmendez.rxstories.model
 
-data class Song (val name: String, val length: String, val songId: Int)
+data class Song(val name: String, val length: String, val songId: Int)
